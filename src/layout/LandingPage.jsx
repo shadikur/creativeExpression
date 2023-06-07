2e9ab27b2ec2e62af132a73ba0c54ab4c8aa4cd1
Mohammad Rahman <me@shadikur.com>
2023-06-07 23:31:47 +0200

    Slider update

--- src/layout/LandingPage.jsx
+++ src/layout/LandingPage.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
+import Container from './Container';
 
 const LandingPage = () => {
     return (
-        <div>
+        <Container>
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
-        </div>
+        </Container>
     );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Slider from '../components/Slider/Slider';
 
 const Home = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <Slider></Slider>
+        </>
     );
 };
 
