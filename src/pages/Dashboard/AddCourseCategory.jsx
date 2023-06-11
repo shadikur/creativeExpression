8801bb41c4f4789dfe6a08a7569d4de34c83bc67
Mohammad Rahman <me@shadikur.com>
2023-06-12 01:47:08 +0200

    Categories updated

--- src/pages/Dashboard/AddCourseCategory.jsx
+++ src/pages/Dashboard/AddCourseCategory.jsx
@@ -1,10 +1,137 @@
-import React from 'react';
+import { Input, Button, Typography, Textarea } from '@material-tailwind/react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { CoreContext } from '../../AppContext/AppContext';
+import useAxios from '../../hooks/useAxios';
+import userMiniSwal from '../../hooks/userMiniSwal';
+import { Alert } from "@material-tailwind/react";
+import { InformationCircleIcon, CloudArrowUpIcon } from "@heroicons/react/24/outline"
+
 
 const AddCourseCategory = () => {
-    return (
-        <div>
 
-        </div>
+    const { register, handleSubmit, getValues, formState: { errors }, control, reset } = useForm();
+    const { uploadImage, photo, setPhoto } = useContext(CoreContext);
+    const axios = useAxios();
+
+    const onSubmit = async (data) => {
+        console.log(data);
+        await axios.post('/categories', data);
+        reset();
+        userMiniSwal(
+            'success',
+            'Category added successfully',
+        );
+
+    };
+
+
+    return (
+        <>
+            <section className="p-8 pb-10 dark:bg-gray-800 dark:text-gray-50 mt-4 bg-white rounded-2xl shadow-xl">
+                {
+                    (Object.keys(errors).length > 0) && (
+                        <div className="flex w-full flex-col gap-2">
+                            <Alert
+                                variant="gradient"
+                                color="red"
+                                icon={
+                                    <InformationCircleIcon
+                                        strokeWidth={2}
+                                        className="h-6 w-6"
+                                    />
+                                }
+                            >
+                                <Typography className="font-medium">Ensure that these requirements are met:</Typography>
+                                <ul className="mt-2 ml-2 list-disc list-inside">
+                                    {errors.name && <li>{errors.name.message}</li>}
+                                    {errors.description && <li>{errors.description.message}</li>}
+                                    {errors.image && <li>{errors.image.message}</li>}
+                                </ul>
+                            </Alert>
+                        </div>
+                    )
+                }
+                <form className="container flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid" onSubmit={handleSubmit(onSubmit)}>
+                    <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-900">
+                        <div className="space-y-2 col-span-full lg:col-span-1">
+                            <p className="font-medium">Category Inormation</p>
+                            <p className="text-xs">Please fillup category of the classes</p>
+                        </div>
+                        <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+                            <div className="col-span-full sm:col-span-3">
+                                <Input
+                                    type="text"
+                                    size="lg"
+                                    label="Category name"
+                                    {...register("name", {
+                                        required: 'Category name is required',
+                                        pattern: {
+                                            value: /^[A-Za-z\s]+$/,
+                                            message: 'Invalid name format'
+                                        },
+                                        minLength: {
+                                            value: 3,
+                                            message: 'Name must be at least 3 characters'
+                                        }
+                                    })}
+                                />
+                            </div>
+                        </div>
+                    </fieldset>
+                    <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-900">
+                        <div className="space-y-2 col-span-full lg:col-span-1">
+                            <p className="font-medium">Description</p>
+                            <p className="text-xs">Please write a brief description which whill be displyed to your website</p>
+                        </div>
+                        <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+                            <div className="col-span-full sm:col-span-3">
+                                <Textarea
+                                    type="text"
+                                    size="lg"
+                                    label="Description"
+                                    {...register("description", {
+                                        required: 'description is required',
+                                        minLength: {
+                                            value: 10,
+                                            message: 'Description text must be at least 10 characters'
+                                        },
+                                        maxLength: {
+                                            value: 1000,
+                                            message: 'Description text must not exceed 1000 characters'
+                                        }
+                                    })}
+                                />
+                            </div>
+                        </div>
+                    </fieldset>
+                    <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-900">
+                        <div className="space-y-2 col-span-full lg:col-span-1">
+                            <p className="font-medium">Profile</p>
+                            <p className="text-xs">Set users profile photo (PhotoURL or Upload)</p>
+                        </div>
+                        <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+                            <div className="col-span-full sm:col-span-3">
+                                <Input
+                                    type="text"
+                                    size="lg"
+                                    label="Photo URL"
+                                    {...register("image", { required: 'Photo URL is required' })}
+                                    value={photo}
+                                    onChange={(e) => setPhoto(e.target.value)}
+                                />
+                            </div>
+                            <div className="col-span-full sm:col-span-3">
+                                <Button variant="gradient" className="flex items-center gap-3" onClick={uploadImage}>
+                                    <CloudArrowUpIcon strokeWidth={2} className="h-5 w-5" /> Upload Photo
+                                </Button>
+                            </div>
+                        </div>
+                    </fieldset>
+                    <Button color="blue" ripple={true} type="submit">Add Category</Button>
+                </form>
+            </section>
+        </>
     );
 };
 
--- src/pages/Dashboard/ViewCourseCategory.jsx
+++ src/pages/Dashboard/ViewCourseCategory.jsx
@@ -1,10 +1,170 @@
 import React from 'react';
+import useAxios from './../../hooks/useAxios';
+import { useQuery, useMutation, QueryClient } from '@tanstack/react-query';
+import LoadingSpinner from '../../components/Spinner/LoadingSpinner';
+import { PencilIcon, UserPlusIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import {
+    Card,
+    CardHeader,
+    Typography,
+    Button,
+    CardBody,
+    Avatar,
+} from "@material-tailwind/react";
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ViewCourseCategory = () => {
-    return (
-        <div>
+    const axios = useAxios();
+    const queryClient = new QueryClient();
+    const { data: categories, isLoading, isError, refetch } = useQuery(
+        {
+            queryKey: ['categories'],
+            queryFn: async () => {
+                const { data } = await axios.get('/categories');
+                return data;
+            }
+        },
+        {
+            onError: (error) => {
+                console.error('Error fetching categories:', error);
+            },
+        }
+    );
 
-        </div>
+    const handleUpdate = async (_id) => {
+        console.log(_id);
+    }
+
+
+    const handleDelete = async (_id) => {
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                try {
+                    axios.delete(`/categories/${_id}`);
+                    refetch();
+                } catch (error) {
+                    console.error('Error deleting category:', error);
+                    throw error;
+                }
+                Swal.fire(
+                    'Deleted!',
+                    'Your category has been deleted.',
+                    'success'
+                )
+            }
+        })
+    };
+
+    if (isLoading) {
+        return <LoadingSpinner />;
+    }
+
+    if (isError) {
+        return <div>Error fetching categories</div>;
+    }
+
+    if (!categories) {
+        return null; // Or show a loading state
+    }
+
+
+    const TABLE_HEAD = ["Category Title", "Descripton", "Image", "Update", "Delete"];
+
+    return (
+        <Card className="w-full mt-5" >
+            <CardHeader floated={false} shadow={false} className="rounded-none">
+                <div className="mb-8 flex items-center justify-between gap-8">
+                    <div>
+                        <Typography variant="h5" color="blue-gray">
+                            List of Categories
+                        </Typography>
+                        <Typography color="gray" className="mt-1 font-normal">
+                            All of our categories are listed here. As an admin, you can add, edit, delete categories.
+                        </Typography>
+                    </div>
+                    <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                        <Link to="/dashboard/addcoursecategory">
+                            <Button className="flex items-center gap-3" color="blue" size="sm">
+                                <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add Categories
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+            </CardHeader>
+            <CardBody className="overflow-hidden px-0">
+                <table className="mt-4 w-full min-w-max table-auto text-left">
+                    <thead>
+                        <tr>
+                            {TABLE_HEAD.map((head) => (
+                                <th key={head} className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal leading-none opacity-70"
+                                    >
+                                        {head}
+                                    </Typography>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {categories.map(({ _id, name, description, image }, index) => {
+                            const isLast = index === categories.length - 1;
+                            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+
+                            return (
+                                <tr key={_id}>
+                                    <td className={classes}>
+                                        <div className="flex items-center gap-3">
+                                            <div className="flex flex-col">
+                                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                                    {name}
+                                                </Typography>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className={classes}>
+                                        <div className="flex flex-col">
+                                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                                {description}
+                                            </Typography>
+
+                                        </div>
+                                    </td>
+                                    <td className={classes}>
+                                        <div className="w-max">
+                                            <img className="w-20 h-20" src={image} alt={name} />
+                                            <code className="text-xs">{image}</code>
+                                        </div>
+                                    </td>
+                                    <td className={classes}>
+                                        <Typography variant="small" color="blue-gray" className="font-normal">
+                                            <Button color="blue" size='sm' onClick={() => handleUpdate(_id)}>Update</Button>
+                                        </Typography>
+                                    </td>
+                                    <td className={classes}>
+                                        <div className='w-[50%] mx-auto'>
+                                            <Button color="blue" size='sm' onClick={() => handleDelete(_id)}>Delete</Button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </CardBody>
+        </Card >
     );
 };
 
